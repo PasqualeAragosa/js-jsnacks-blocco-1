6b935e49c27b78ddc570e54596ac6f78c2858c81
Pasquale Aragosa <aragosap@gmail.com>
2022-10-12 12:11:15 +0200

    add file js

--- jsnack_2/assets/js/main.js
+++ jsnack_2/assets/js/main.js
@@ -1,18 +1,28 @@
 /* 
-
+Snack 2
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga
 */
 
 const mainEl = document.getElementById('site_main');
-const num1 = prompt("Inserisci il primo numero: ");
-const num2 = prompt("Inserisci il secondo numero: ");
+const first_word = prompt('Inserisci la prima parola: ');
+const second_word = prompt('Inserisci la seconda parola: ');
+console.log(first_word, second_word);
+let more;
+let minus;
 
-if (num1 > num2) {
-    mainEl.innerHTML = "Il primo numero è quello maggiore";
-    console.log('Num1: ' + num1 + ' è maggiore di Num2: ' + num2);
-} else if(num2 > num1) {
-    mainEl.innerHTML = "Il secondo numero è quello maggiore";
-    console.log('Num2: ' + num2 + ' è maggiore di Num1: ' + num1);
+
+if (first_word.length > second_word.length) {
+    more = first_word;
+    minus = second_word;
+    mainEl.append(more);
+    mainEl.append(minus);
+} else if(second_word.length > first_word.length) {
+    more = second_word;
+    minus = first_word;
+    mainEl.append(more);
+    mainEl.append(minus);
 } else {
-    mainEl.innerHTML = "Pareggio";
-    console.log('Num1: ' + num1 + ' è uguale a Num2: ' + num2);
+    mainEl.innerHTML = "Le parole hanno la stessa lunghezza";
+    //console.log('Num1: ' + num1 + ' è uguale a Num2: ' + num2);
 }
