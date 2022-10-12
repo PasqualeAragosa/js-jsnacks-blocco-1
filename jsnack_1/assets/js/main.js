1a8d4a8fe496803cb55facf65efd0ecf88c37cb7
Pasquale Aragosa <aragosap@gmail.com>
2022-10-12 11:42:37 +0200

    add console.log

--- jsnack_1/assets/js/main.js
+++ jsnack_1/assets/js/main.js
@@ -10,8 +10,11 @@ const num2 = prompt("Inserisci il secondo numero: ");
 
 if (num1 > num2) {
     mainEl.innerHTML = "Il primo numero è quello maggiore";
+    console.log('Num1: ' + num1 + ' è maggiore di Num2: ' + num2);
 } else if(num2 > num1) {
     mainEl.innerHTML = "Il secondo numero è quello maggiore";
+    console.log('Num2: ' + num2 + ' è maggiore di Num1: ' + num1);
 } else {
     mainEl.innerHTML = "Pareggio";
+    console.log('Num1: ' + num1 + ' è uguale a Num2: ' + num2);
 }
