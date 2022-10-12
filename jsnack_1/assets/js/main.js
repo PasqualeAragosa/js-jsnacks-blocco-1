0f595fd740e3e239e594b1fb1c06b8cf6647430e
Pasquale Aragosa <aragosap@gmail.com>
2022-10-12 11:53:08 +0200

    add variabile a video

--- jsnack_1/assets/js/main.js
+++ jsnack_1/assets/js/main.js
@@ -9,10 +9,10 @@ const num1 = prompt("Inserisci il primo numero: ");
 const num2 = prompt("Inserisci il secondo numero: ");
 
 if (num1 > num2) {
-    mainEl.innerHTML = "Il primo numero è quello maggiore";
+    mainEl.innerHTML = "Il primo numero è quello maggiore: " + num1;
     console.log('Num1: ' + num1 + ' è maggiore di Num2: ' + num2);
 } else if(num2 > num1) {
-    mainEl.innerHTML = "Il secondo numero è quello maggiore";
+    mainEl.innerHTML = "Il secondo numero è quello maggiore: " + num2;
     console.log('Num2: ' + num2 + ' è maggiore di Num1: ' + num1);
 } else {
     mainEl.innerHTML = "Pareggio";
